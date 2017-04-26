af4bacc957d322879cf961d6de9d29f4177fceac
LiMengrun <MengrunLi@wl-10-190-90-166.wireless.duke.edu>
2017-04-25 22:32:46 -0400

    ..

--- model.R
+++ model.R
@@ -6,7 +6,7 @@ library(stringr)
 
 trip = fread('babs_open_data_year_2/201508_trip_data.csv')
 station = fread('babs_open_data_year_2/201508_station_data.csv')
-status <-fread("babs_open_data_year_2/201508_status_data.csv")
+#status <-fread("babs_open_data_year_2/201508_status_data.csv")
 head(trip)
 df = trip %>% 
   mutate(date = floor_date(mdy_hm(`Start Date`),'day')) %>%
@@ -16,3 +16,30 @@ df = trip %>%
 
 agg = merge(df, station, by.x = 'Start Terminal', by.y = 'station_id') %>%
   mutate(day_of_week = wday(date))
+
+
+library(fields)
+library(maps)
+states <- map_data("state")
+bayArea <- subset(states, region %in% c("california"))
+
+counties <- map_data("county")
+ca_county <- subset(counties, region == "california")
+bay_name <- c("san francisco", "san mateo","santa clara")
+bay_county <- subset(ca_county,subregion %in% bay_name)
+
+r = raster(nrows=50, ncol=100,
+           xmn = min(bay_county$long), xmx = max(bay_county$long),
+           ymn = min(bay_county$lat), ymx = max(bay_county$lat))
+bay = rasterize(bay_county[,c('long','lat')],r)
+cells = which(!is.na(bay[]))
+pred_coords = xyFromCell(r, cells)
+coords = dplyr::select(agg, long, lat) %>% as.matrix()
+tps = Tps(x = coords, Y=agg$`Number of Trips`)
+pm25_pred = r
+pm25_pred[cells] = predict(tps, pred_coords)
+
+plot(pm25_pred)
+points(coords, pch=16, cex=0.5)
+#library(ggplot2)
+#ggplot(pm25_pred, aes(long,lat))+geom_raster(aes(fill = values))
